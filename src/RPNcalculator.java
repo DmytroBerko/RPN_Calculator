7cc97bc84841619396e0e31c573a08fd688e780f
Dmytro Berko <dmytro.berko@pm.me>
2018-08-16 20:51:49 +0200

    RPN calculator implemented

--- src/RPNcalculator.java
+++ src/RPNcalculator.java
@@ -13,13 +13,37 @@ public static int calculate(String ops[]){
 	int x,y;
 	String operators = new String("+-/*");
 	// Define a Stack
-	
-	for (String op: ops){
-		// Implement here your algorithm
+	Stack st = new Stack();
+	for (String op: ops) {
+		if (op.compareTo("+") != 0 &&
+			op.compareTo("-") != 0 &&
+			op.compareTo("/") != 0 &&
+			op.compareTo("*") != 0) {
+			st.push(op);
+		} else {
+			y = Integer.parseInt(st.pop().toString());
+			x = Integer.parseInt(st.pop().toString());
+			switch (op) {
+				case "+":
+					st.push(x + y);
+					break;
+				case "-":
+					st.push(x - y);
+					break;
+				case "/":
+					st.push(x / y);
+					break;
+				case "*":
+					st.push(x * y);
+					break;
+			}
+		}
 	}
-	
-	// if the stack is not empty, result= stack.pop()
-	// else result = 0
+	if (!st.empty()) {
+		result = (int) st.pop();
+	} else {
+		result = 0;
+	}
 	return result;
 }
 public static void main(String args[]){
@@ -33,7 +57,12 @@ public static void main(String args[]){
 	
 	String[] argu= new String[]{"5","3","+"};
 	int result = calculate(argu);
+	System.out.println(result);
 	System.out.println("5"+" 3"+" +"+" ="+ result);
+	
+	result = calculate(new String[]{"6","3","/"});
+	System.out.println("6"+" 3"+" /"+" = "+ result);
+	
 	result = calculate(new String[]{"5","3","-"});
 	System.out.println("5"+" 3"+" -"+" = "+ result);
 	result = calculate(new String[]{"2","1","12","3","/", "-", "+"});
@@ -41,5 +70,6 @@ public static void main(String args[]){
 	result = calculate(new String[]{"3", "2", "*", "11", "-"});
 	System.out.println("3"+" 2"+" *"+" 11"+" -"+" = "+ result);
 	
+	System.out.println("this is " + "+".compareTo("+"));
 }
 }
